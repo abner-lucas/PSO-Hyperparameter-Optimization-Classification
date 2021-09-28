8ce7628e71ad2947a8abb29290b83ab18618ef9f
Abner Lucas <abnerlucas.cad@gmail.com>
2021-09-28 19:23:48 -0300

    Feedfoward com otimização de pesos e bias com PySwarms

--- outros/mnist_loader.py
+++ outros/mnist_loader.py
@@ -8,7 +8,7 @@ import gzip
 import numpy as np
 
 def load_data():
-    f = gzip.open('data/mnist.pkl.gz', 'rb')
+    f = gzip.open('mnist_dataset/mnist.pkl.gz', 'rb')
     training_data, validation_data, test_data = pickle.load(f, encoding="latin1")
     f.close()
     return (training_data, validation_data, test_data)
