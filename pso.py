4c7c80d3767e21bd764ce191e62df16d6452dc84
Abner Lucas <abnerlucas.cad@gmail.com>
2021-09-29 01:33:24 -0300

    formatando com Jupyter

--- pso.py
+++ pso.py
@@ -8,7 +8,7 @@ class Particle(object):
 
     def reset(self, dimensions, lower_bound, upper_bound, objective_function):
         position = []
-        for i in xrange(dimensions):
+        for i in range(dimensions):
             if lower_bound[i] < upper_bound[i]:
                 position.extend(np.random.randint(lower_bound[i], upper_bound[i] + 1, 1, dtype=int))
             elif lower_bound[i] == upper_bound[i]:
@@ -28,15 +28,6 @@ class Particle(object):
         self.best_function_value = self.function_value[:]
 
     def update_velocity(self, omega, phip, phig, best_swarm_position):
-        """Particle velocity update
-
-		Args:
-			omega (float): Velocity equation constant.
-			phip (float): Velocity equation constant.
-			phig (float): Velocity equation constant.
-			best_swarm_position (np.array): Best particle position.
-
-        """
         random_coefficient_p = np.random.uniform(size=np.asarray(self.position[-1]).shape)
         random_coefficient_g = np.random.uniform(size=np.asarray(self.position[-1]).shape)
 
@@ -54,14 +45,6 @@ class Particle(object):
         self.velocity[-1] = self.velocity[-1].astype(int)
 
     def update_position(self, lower_bound, upper_bound, objective_function):
-        """Particle position update
-
-		Args:
-			lower_bound (np.array): Vector of lower boundaries for particle dimensions.
-			upper_bound (np.array): Vector of upper boundaries for particle dimensions.
-			objective function (function): Black-box function to evaluate.
-
-        """
         new_position = self.position[-1] + self.velocity[-1]
 
         if np.array_equal(self.position[-1], new_position):
@@ -82,17 +65,7 @@ class Particle(object):
             self.best_function_value.append(self.function_value[-1])
 
 class Pso(object):
-    """PSO wrapper
-
-    This class contains the particles and provides an abstraction to hold all the context
-    of the PSO algorithm
-
-    Args:
-        swarmsize (int): Number of particles in the swarm
-        maxiter (int): Maximum number of generations the swarm will run
-
-    """
-    def __init__(self, swarmsize=100, maxiter=100):
+    def __init__(self, swarmsize, maxiter):
         self.max_generations = maxiter
         self.swarmsize = swarmsize
 
@@ -111,19 +84,6 @@ class Pso(object):
         self.retired_particles = []
 
     def run(self, function, lower_bound, upper_bound, kwargs=None):
-        """Perform a particle swarm optimization (PSO)
-
-		Args:
-			objective_function (function): The function to be minimized.
-			lower_bound (np.array): Vector of lower boundaries for particle dimensions.
-			upper_bound (np.array): Vector of upper boundaries for particle dimensions.
-
-		Returns:
-			best_position (np.array): Best known position
-			accuracy (float): Objective value at best_position
-			:param kwargs:
-
-        """
         if kwargs is None:
             kwargs = {}
 
@@ -180,20 +140,9 @@ class Pso(object):
                              upper_bound,
                              dimensions,
                              objective_function):
-        """Initializes the particles for the swarm
 
-		Args:
-			objective_function (function): The function to be minimized.
-			lower_bound (np.array): Vector of lower boundaries for particle dimensions.
-			upper_bound (np.array): Vector of upper boundaries for particle dimensions.
-			dimensions (int): Number of dimensions of the search space.
-
-		Returns:
-			particles (list): Collection or particles in the swarm
-
-        """
         particles = []
-        for _ in xrange(self.swarmsize):
+        for _ in range(self.swarmsize):
             particles.append(Particle(lower_bound,
                                       upper_bound,
                                       dimensions,
